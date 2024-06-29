7ee1bd3f8b00da10d651219617bee3f57d88a05f
pocket-dimen <nh.muhammadkhalidsayfullah@gmail.com>
2024-06-29 13:19:36 +0100

    Version 1.0.9 released

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "app.tabletracker"
         minSdk = 24
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.0.8"
+        versionCode = 10
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
